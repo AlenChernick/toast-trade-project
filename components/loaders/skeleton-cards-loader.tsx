4dfd70d7ef194bc881889e575d3f605b701efe9c
AlenChernick <crazyguy771@gmail.com>
2024-05-04 23:43:17 +0300

    fixed dashboard auctions cards skeleton

--- components/loaders/skeleton-cards-loader.tsx
+++ components/loaders/skeleton-cards-loader.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 const SkeletonCardsLoader = () => {
   return (
     <section className='grid md:grid-cols-5 gap-4'>
-      <div className='w-[320px] max-w-[260px] bg-card rounded-md'>
+      <div className='md:w-[320px] w-[250px] bg-card rounded-md'>
         <div className='p-4'>
           <Skeleton className='md:text-base text-xl h-8 w-3/4 mb-2' />
           <Skeleton className='h-6 w-1/2 mb-2' />
@@ -14,14 +14,14 @@ const SkeletonCardsLoader = () => {
           <Skeleton className='h-6 w-3/4 mb-2' />
         </div>
         <div className='p-4 py-1'>
-          <Skeleton className='object-contain rounded-md h-64 w-full' />
+          <Skeleton className='object-contain rounded-md h-96 w-full' />
         </div>
         <div className='p-4 flex justify-between'>
           <Skeleton className='w-1/4 h-10' />
           <Skeleton className='w-1/4 h-10' />
         </div>
       </div>
-      <div className='w-[320px] max-w-[260px] bg-card rounded-md'>
+      <div className='md:w-[320px] w-[250px] bg-card rounded-md'>
         <div className='p-4'>
           <Skeleton className='md:text-base text-xl h-8 w-3/4 mb-2' />
           <Skeleton className='h-6 w-1/2 mb-2' />
@@ -32,14 +32,14 @@ const SkeletonCardsLoader = () => {
           <Skeleton className='h-6 w-3/4 mb-2' />
         </div>
         <div className='p-4 py-1'>
-          <Skeleton className='object-contain rounded-md h-64 w-full' />
+          <Skeleton className='object-contain rounded-md h-96 w-full' />
         </div>
         <div className='p-4 flex justify-between'>
           <Skeleton className='w-1/4 h-10' />
           <Skeleton className='w-1/4 h-10' />
         </div>
       </div>
-      <div className='w-[320px] max-w-[260px] bg-card rounded-md'>
+      <div className='md:w-[320px] w-[250px] bg-card rounded-md'>
         <div className='p-4'>
           <Skeleton className='md:text-base text-xl h-8 w-3/4 mb-2' />
           <Skeleton className='h-6 w-1/2 mb-2' />
@@ -50,14 +50,14 @@ const SkeletonCardsLoader = () => {
           <Skeleton className='h-6 w-3/4 mb-2' />
         </div>
         <div className='p-4 py-1'>
-          <Skeleton className='object-contain rounded-md h-64 w-full' />
+          <Skeleton className='object-contain rounded-md h-96 w-full' />
         </div>
         <div className='p-4 flex justify-between'>
           <Skeleton className='w-1/4 h-10' />
           <Skeleton className='w-1/4 h-10' />
         </div>
       </div>
-      <div className='w-[320px] max-w-[260px] bg-card rounded-md'>
+      <div className='md:w-[320px] w-[250px] bg-card rounded-md'>
         <div className='p-4'>
           <Skeleton className='md:text-base text-xl h-8 w-3/4 mb-2' />
           <Skeleton className='h-6 w-1/2 mb-2' />
@@ -68,7 +68,7 @@ const SkeletonCardsLoader = () => {
           <Skeleton className='h-6 w-3/4 mb-2' />
         </div>
         <div className='p-4 py-1'>
-          <Skeleton className='object-contain rounded-md h-64 w-full' />
+          <Skeleton className='object-contain rounded-md h-96 w-full' />
         </div>
         <div className='p-4 flex justify-between'>
           <Skeleton className='w-1/4 h-10' />
