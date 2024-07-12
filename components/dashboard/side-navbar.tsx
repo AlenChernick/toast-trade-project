5a65c02c2d571404bab778e9050d223917531e7e
AlenChernick <crazyguy771@gmail.com>
2024-07-13 02:37:00 +0300

    enchantment/make the scroll area more smooth in mobile view

--- components/dashboard/side-navbar.tsx
+++ components/dashboard/side-navbar.tsx
@@ -1,4 +1,5 @@
-import type { FC } from 'react';
+'use client';
+import { useLayoutEffect, useRef, type FC } from 'react';
 import { AppRoutes, DashboardActionType } from '@/enum';
 import {
   CirclePlus,
@@ -15,18 +16,40 @@ const SideNavbar: FC<{
   type: string | string[] | undefined;
   userId: string;
 }> = ({ isEdit, type, userId }) => {
+  const refs = {
+    dashboardRef: useRef<HTMLAnchorElement>(null),
+    auctionsRef: useRef<HTMLAnchorElement>(null),
+    bidsRef: useRef<HTMLAnchorElement>(null),
+    createEditAuctionRef: useRef<HTMLAnchorElement>(null),
+    settingsRef: useRef<HTMLAnchorElement>(null),
+  };
+
+  useLayoutEffect(() => {
+    const activeRef = Object.values(refs).find((ref) =>
+      ref.current?.classList.contains('nav-active')
+    );
+    if (activeRef && activeRef.current) {
+      activeRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+      });
+    }
+  }, [type]);
+
   return (
     <ScrollArea
-      className='overflow-x-hidden h-14 md:h-auto whitespace-nowrap flex border-b-2
+      className='overflow-x-hidden h-12 md:h-auto whitespace-nowrap flex border-b-2
      md:min-w-52 md:border-r-2 md:border-b-0
       border-primary mb-4 md:-mx-4 md:mr-5'>
-      <nav className='flex flex-row md:flex-col gap-5 md:gap-2'>
+      <nav className='flex flex-row md:flex-col gap-1 md:gap-2'>
         <Link
           href={`${AppRoutes.Dashboard}/${userId}`}
           title='Dashboard'
           className={`${
             type === undefined ? 'nav-active' : 'side-nav-text'
-          } flex gap-2 items-center`}>
+          } flex gap-2 items-center`}
+          ref={refs.dashboardRef}>
           <LayoutDashboard className='w-4 h-4' />
           Dashboard
         </Link>
@@ -37,7 +60,8 @@ const SideNavbar: FC<{
             type === DashboardActionType.Auctions
               ? 'nav-active'
               : 'side-nav-text'
-          } flex gap-2 items-center`}>
+          } flex gap-2 items-center`}
+          ref={refs.auctionsRef}>
           <Columns3 className='w-4 h-4' />
           Auctions
         </Link>
@@ -46,7 +70,8 @@ const SideNavbar: FC<{
           title='User bids'
           className={`${
             type === DashboardActionType.Bids ? 'nav-active' : 'side-nav-text'
-          } flex gap-2 items-center`}>
+          } flex gap-2 items-center`}
+          ref={refs.bidsRef}>
           <SquareActivity className='w-4 h-4' />
           Bids
         </Link>
@@ -57,7 +82,8 @@ const SideNavbar: FC<{
             type === DashboardActionType.CreateOrEditAuction
               ? 'nav-active'
               : 'side-nav-text'
-          } flex gap-2 items-center`}>
+          } flex gap-2 items-center`}
+          ref={refs.createEditAuctionRef}>
           <CirclePlus className='w-4 h-4' />
           {`${isEdit ? 'Edit' : 'Create'} auction`}
         </Link>
@@ -69,7 +95,8 @@ const SideNavbar: FC<{
             type === DashboardActionType.Settings
               ? 'nav-active'
               : 'side-nav-text'
-          } flex gap-2 items-center`}>
+          } flex gap-2 items-center`}
+          ref={refs.settingsRef}>
           <UserRoundCog className='w-4 h-4' />
           Settings
         </Link>
